04525c39ba6a8dae5a91764b4de5b73e851d7b57
simaski <jimmyhernandezu@gmail.com>
2016-11-14 18:51:46 -0300

    14 noviembre 2016

--- app/src/main/java/com/rinno/simaski/pruebaconstraint/MainActivity.java
+++ app/src/main/java/com/rinno/simaski/pruebaconstraint/MainActivity.java
@@ -1,13 +1,31 @@
 package com.rinno.simaski.pruebaconstraint;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.btEntrar)
+    Button btEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btEntrar)
+    public void onClick() {
+        Intent miIntent = new Intent(MainActivity.this, SonyActivity.class);
+        MainActivity.this.startActivity(miIntent);
+        MainActivity.this.finish();
     }
 }
---
+++ app/src/main/java/com/rinno/simaski/pruebaconstraint/SonyActivity.java
@@ -0,0 +1,13 @@
+package com.rinno.simaski.pruebaconstraint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SonyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sony_xperia_x);
+    }
+}
